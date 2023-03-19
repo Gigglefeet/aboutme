399d31eecb2446230c15ed126773de1cde57de91
robertmariuson <robert.mariuson@ebcont.com>
2023-03-19 10:05:14 +0100

    First basic outline (without end goal flex)

---
+++ about-me-app/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div>Here is header</div>
+      </header>
+      <body className="Main">
+        <p>This is the Bodies P</p>
+        <div>This is a div in the body</div>
+      </body>
+      <footer>
+        <p className="text-in-footer">This is the Footer P</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
